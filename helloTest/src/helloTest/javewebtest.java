100a652d0fecbab724e6db15b1046e855cf313d6
Tan Nguyen <Tan Nguyen@DESKTOP-2594HO3>
2024-03-24 13:42:19 +0700

    version2

--- helloTest/src/helloTest/javewebtest.java
+++ helloTest/src/helloTest/javewebtest.java
@@ -2,7 +2,8 @@ package helloTest;
 
 public class javewebtest {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("helloannfdklnlsk");
+		System.out.println("helloannfdklnlsk");
 		
 }
 
